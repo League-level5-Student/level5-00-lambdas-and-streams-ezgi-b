d19c7d43952e2e85e39f4322655d4b89eda6674b
ezgi-b <52934686+ezgi-b@users.noreply.github.com>
2021-08-30 19:13:34 -0700

    I learned about streams and finished this module

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -21,11 +21,46 @@ public class LambdaMethods {
 			System.out.println(s);
 		}, "A String.");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
+		printCustomMessage((e)->{
+			int count = 0;
+			String s = "";
+			for(int i = 0; i<e.length(); i++) {
+				if(Character.isLetter(e.charAt(i))) {
+					if(count%2 == 0) {
+						s += e.substring(i, i+1).toLowerCase();
+					}else {
+						s += e.substring(i, i+1).toUpperCase();
+					}
+					count++;
+				}else {
+					s += e.substring(i, i+1);
+				}
+			}
+			System.out.println(s);
+		}, "I like cookies a lot!");
 		
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((e)->{
+			String s = "";
+			for(int i = 0; i<e.length(); i++) {
+				if(i!=e.length()-1) {
+					s += e.charAt(i) + ".";
+				}else {
+					s += e.charAt(i);
+				}
+			}
+			System.out.println(s);
+		}, "Platypuses are mammals.");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((e)->{
+			String s = "";
+			for(int i = 0; i<e.length(); i++) {
+				if(!(e.substring(i, i+1).toLowerCase().equals("e")||e.substring(i, i+1).toLowerCase().equals("i")||e.substring(i, i+1).toLowerCase().equals("o")||e.substring(i, i+1).toLowerCase().equals("u")||e.substring(i, i+1).toLowerCase().equals("a"))) {
+					s += e.charAt(i);
+				}
+			}
+			System.out.println(s);
+		}, "Onomatopoeias are cool.");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
